030b357f223a1d9d4f21b63e97bc82fa5e50640f
Philippos Slicher <philipposslicher@gmail.com>
2015-11-15 12:23:59 +0400

    Small stuff, added IBOutlet

--- Gesturizer/ViewController.swift
+++ Gesturizer/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gesture: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
