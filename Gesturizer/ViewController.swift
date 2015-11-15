f0284cffee6b277626c7898a742c874aea24a2cd
Philippos Slicher <philipposslicher@gmail.com>
2015-11-15 12:36:41 +0400

    Added tapGesutureRecognizer

--- Gesturizer/ViewController.swift
+++ Gesturizer/ViewController.swift
@@ -25,5 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func singleTap(sender: AnyObject) {
+    }
+    
 }
 
