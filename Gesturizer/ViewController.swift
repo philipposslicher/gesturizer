c887d705d98e49018f04859f3e73805c7f013978
Philippos Slicher <philipposslicher@gmail.com>
2015-11-19 11:48:51 +0400

    Trying to fix something....still broken

--- Gesturizer/ViewController.swift
+++ Gesturizer/ViewController.swift
@@ -2,15 +2,25 @@ import UIKit
 
 class ViewController: UIViewController {//
 
-    @IBOutlet weak var gestureName: UILabel!
+    @IBOutlet weak var gestureName: UILabel
+    
+    @IBAction func singleTap(sender: AnyObject) {
+        //showGestureName("Tap")
+    }
     
     
-    
+    func showGestureName(name: String) {
+        self.gestureName.text = name
+        UIView.animateWithDuration(1.0,
+            animations: { self.gestureName.alpha = 1.0 },
+            completion: { (finished) -> Void in
+                self.gestureName.alpha = 0
+        })
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -18,21 +28,7 @@ class ViewController: UIViewController {//
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func singleTap(sender: UITapGestureRecognizer) {
-        //showGestureName("Tap")
-    }
 
-    
-    /*
-    func showGestureName(name: String) {
-        gestureName.text = name
-        gestureName.hidden = false
-        UIView.animateWithDuration(1.0,
-            animations: { self.gestureName.alpha = 1.0 },
-            completion: { _ in
-                UIView.animateWithDuration(1.0) { self.gestureName.alpha = 0 }
-        })
-    }
-    */
+
 
 }//
